444ca0befc9dad49e9ded6efc433a4dbbc84118e
Jay <im_blacktastic@yahoo.com>
2018-07-13 22:45:37 +0000

    autoscroll added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom(){
+    //selectors
+    var messages = $("#messages");
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    
+    
+    if(clientHeight + scrollTop + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
 socket.on('connect', function(){
    console.log("connected to server"); 
    
@@ -20,7 +38,7 @@ socket.on('newMsg', function(data){
         time: formattedTime
     });
     $('#messages').append(html);
-    
+    scrollToBottom();
 });
 
 socket.on('newPosMsg', function(data){
@@ -33,6 +51,7 @@ socket.on('newPosMsg', function(data){
         time: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 async function sendMessage(text, from){
