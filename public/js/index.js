655eec116238bd68c97b9bfd2b319c129961fa79
Jay <im_blacktastic@yahoo.com>
2018-07-13 17:53:02 +0000

    socket io added, simple emit and listers set up and tested

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+socket.on('connect', function(){
+   console.log("connected to server"); 
+   sendMessage("User has joined the chat", "jay")
+   
+});
+
+socket.on('disconnect', function(){
+   console.log("Disconnected from server"); 
+});
+
+socket.on('newMsg', function(data){
+    console.log('Message Received:', data);
+});
+
+function sendMessage(text, from){
+    socket.emit('sendMsg', {
+      text: text,
+      from: from
+   });
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,36 @@
 const path                  = require("path");                                          
 const publicPath            = path.join(__dirname, "../public");
 const express               = require("express");
-var app                     = express();
 const port                  = process.env.PORT;
+const socketIO              = require("socket.io");
+const http                  = require("http");
+
+var app                     = express();
+var server                  = http.createServer(app);
+var io                      = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', function(socket){
+    console.log("New user connected");
+    
+    socket.emit('newMsg', {
+        text: "Welcome to the chat",
+        from: "system",
+        time:  new Date().getHours() + ':' + new Date().getMinutes()
+    });
+    
+    socket.on('sendMsg', function(data) {
+        console.log('sendMsg:', data);
+    });
+    
+    socket.on('disconnect', function(socket){
+        console.log("Client disconnected");
+    });
+});
+
+
+
 
 app.get('/', function(req, res){
    res.render('index.html'); 
@@ -17,6 +43,6 @@ app.get('/', function(req, res){
 
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+server.listen(process.env.PORT, process.env.IP, function(){
     console.log("node-chat-app listner Service spinning, up. We're in the pipe, 5 by 5!");
 });
